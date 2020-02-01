fb6049dc2a40b0d7df6f4279c7aee7d27bc074d6
Anselm Binninger <anselm.binninger@bwk-technik.de>
2020-02-01 22:09:18 +0100

    Add bill url to mgmt view

--- billing/urls.py
+++ billing/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from billing.views import GeneratePDF, HTMLPreview
 
 __author__ = ''
+app_name = 'billing'
 
 urlpatterns = [
     url(r'^bill/(?P<order>[\S0-9_.-\\s\- ]*)$', GeneratePDF.as_view(), name='invoice_pdf'),
