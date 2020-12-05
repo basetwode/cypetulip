8090468d91a3b1d6b92c82c55c869557bea0325d
Anselm Binninger <anselm92@gmail.com>
2020-12-05 16:06:04 +0100

    Refactor mail mixin

--- utils/mixins.py
+++ utils/mixins.py
@@ -14,6 +14,16 @@ from management.models import LegalSetting, MailSetting
 from shipping.models import OnlineShipment
 from shop.models import Order
 
+class EmailLogMixin:
+
+    def log(self, **kwargs):
+        pass
+        # log = CommunicationLog(
+        #     contact=
+        #     kwargs
+        # )
+
+
 
 class EmailMixin:
     email_template = ''
@@ -42,78 +52,76 @@ class EmailMixin:
         )
 
     def send_mail(self, receiver_user, subject, content, context):
-        EmailThread(receiver_user, content, subject, context, self.get_template(), self.connection()).start()
+        mail_thread = EmailThread(receiver_user, content, subject, context, self.get_template(), self.connection())
+        mail_thread.create_mail()
+        mail_thread.start()
 
-
-class EmailThread():
+class EmailThread(threading.Thread):
     def __init__(self, receiver_user, content, subject, context, email_template, connection):
+        super(EmailThread, self).__init__()
         self.subject = subject
         self.context = context
         self.content = content
         self.email_template = email_template
         self.receiver_user = receiver_user
         self.connection = connection
+        self.email = None
 
-    def start(self):
-
+    def create_mail(self):
         result = None
         tries = 0
         #while (result is None or result is not 1) and tries < 5:
+
+
+        print("Sending new email to " + self.receiver_user.email)
+        legal = LegalSetting.objects.first()
+        self.context['content'] = self.content
+        self.context['legal'] = legal
+        html_content = render_to_string(self.email_template, context=self.context)
+        print(self.content)
+
+        email = EmailMultiAlternatives('Subject', self.subject, connection=self.connection)
+        email.subject = self.subject
+        email.mixed_subtype = 'related'
+        email.content_subtype = 'html'
+        email.attach_alternative(html_content, "text/html")
+        email.to = [self.receiver_user.email]
+
+        logo_file = legal.logo.open("rb")
         try:
+            logo = MIMEImage(logo_file.read())
+            logo.add_header('Content-ID', '<{}>'.format(legal.logo.name))
+            email.attach(logo)
+        finally:
+            logo_file.close()
 
-            print("Sending new email to " + self.receiver_user.email)
-            legal = LegalSetting.objects.first()
-            self.context['content'] = self.content
-            self.context['legal'] = legal
-            html_content = render_to_string(self.email_template, context=self.context)
-            print(self.content)
+        if 'files' in self.context and self.context['files']:
+            for file_name, file in self.context['files'].items():
+                email.attach(file_name, file, )
 
-            email = EmailMultiAlternatives('Subject', self.subject, connection=self.connection)
-            email.subject = self.subject
-            email.mixed_subtype = 'related'
-            email.content_subtype = 'html'
-            email.attach_alternative(html_content, "text/html")
-            email.to = [self.receiver_user.email]
+        if 'object' in self.context and isinstance(self.context['object'], OnlineShipment):
+            email.attach_file(self.context['object'].file.path)
 
-            logo_file = legal.logo.open("rb")
-            try:
-                logo = MIMEImage(logo_file.read())
-                logo.add_header('Content-ID', '<{}>'.format(legal.logo.name))
-                email.attach(logo)
-            finally:
-                logo_file.close()
+        if 'object' in self.context and isinstance(self.context['object'], Order):
+            for order_item in self.context['object'].orderitem_set.all():
+                if hasattr(order_item.product,'product') and order_item.product.product.product_picture:
+                    product_file = order_item.product.product.product_picture.open("rb")
+                    try:
+                        product_img = MIMEImage(product_file.read())
+                        product_img.add_header('Content-ID', '<{}>'
+                                               .format(order_item.product.product.product_picture.name))
+                        email.attach(product_img)
+                    finally:
+                        product_file.close()
 
-            if 'files' in self.context and self.context['files']:
-                for file_name, file in self.context['files'].items():
-                    email.attach(file_name, file, )
+        print("from " + email.from_email)
+        print("Sending mail")
+        tries += 1
+        self.email = email
 
-            if 'object' in self.context and isinstance(self.context['object'], OnlineShipment):
-                email.attach_file(self.context['object'].file.path)
-
-            if 'object' in self.context and isinstance(self.context['object'], Order):
-                for order_item in self.context['object'].orderitem_set.all():
-                    if hasattr(order_item.product,'product') and order_item.product.product.product_picture:
-                        product_file = order_item.product.product.product_picture.open("rb")
-                        try:
-                            product_img = MIMEImage(product_file.read())
-                            product_img.add_header('Content-ID', '<{}>'
-                                                   .format(order_item.product.product.product_picture.name))
-                            email.attach(product_img)
-                        finally:
-                            product_file.close()
-
-            print("from " + email.from_email)
-            print("Sending mail")
-            tries += 1
-            result = email.send()
-            print(result)
-        except Exception as e:
-            print(e)
-            print("Error when sending mail... retrying")
-            #time.sleep(15)
-
-        return result
-
+    def run(self):
+        result =  self.email.send()
+        print("Mail sent: "+ str(result))
 
 class PaginatedFilterViews(View):
     def get_context_data(self, **kwargs):
