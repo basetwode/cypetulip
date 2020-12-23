a9537d965c308fca46976dea7f0da243d0b4d924
Anselm Binninger <anselm92@gmail.com>
2020-12-23 10:22:29 +0100

    Enable caching for cms and shop product pages

--- cms/urls.py
+++ cms/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
 
 from cms.views import *
 
@@ -11,9 +12,9 @@ urlpatterns = [
     url(r'^permissions-denied/$', PermissionDeniedView.as_view(), name='permissions_denied'),
     url(r'^theme.css$', CSSView.as_view()),
     url(r"contact/$", ContactView.as_view(), name="contact"),
-    url(r"legal/$", LegalView.as_view(), name="legal"),
-    url(r"general-business-terms/$", GBTView.as_view(), name="gbt"),
-    url(r"cancellation-policy/$", CancellationPolicyView.as_view(), name="cancellation-policy"),
-    url(r"privacy-policy/$", PrivacyPolicyView.as_view(), name="privacy-policy"),
-    url(r"^(?P<site>[a-zA-Z0-9_.-]+)/$", GenericView.as_view(), name="generic_cms_page")
+    url(r"legal/$", cache_page(60*60)(LegalView.as_view()), name="legal"),
+    url(r"general-business-terms/$", cache_page(60*60)(GBTView.as_view()), name="gbt"),
+    url(r"cancellation-policy/$", cache_page(60*60)(CancellationPolicyView.as_view()), name="cancellation-policy"),
+    url(r"privacy-policy/$", cache_page(60*60)(PrivacyPolicyView.as_view()), name="privacy-policy"),
+    url(r"^(?P<site>[a-zA-Z0-9_.-]+)/$", cache_page(60*60)(GenericView.as_view()), name="generic_cms_page")
 ]
